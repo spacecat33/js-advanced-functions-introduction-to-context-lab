0bce4e1ac224823db95f4ba1fd0468bbbae00974
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-22 16:26:20 +0000

    commit tree d53fa6f2732149a89cc6a034494eb20ee1146661

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+
+// createEmployeeRecord
+
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
